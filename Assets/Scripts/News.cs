3f9115f5bc7816a3cb7a7ae46896f1d762a1eb2c
Михаил <77919295+Misha1302@users.noreply.github.com>
2022-10-14 23:26:22 +0300

    v1
    added news.txt,
    background and news field,
    start make NewsUpdate

--- Assets/Scripts/News.cs
+++ Assets/Scripts/News.cs
@@ -1,7 +1,27 @@
+using System.Collections;
+using System.IO;
 using TMPro;
 using UnityEngine;
 
 public class News : MonoBehaviour
 {
+    [SerializeField] private TMP_Text newsText;
+    [SerializeField] private Transform newsTextTransform;
+    [SerializeField] private float newsWaitTime;
 
+    private string[] _newsLines;
+
+    private void Start()
+    {
+        _newsLines = File.ReadAllText(@"src\News.txt").Split('\n');
+        StartCoroutine(NewsUpdate());
+    } // ReSharper disable IteratorNeverReturns
+    private IEnumerator NewsUpdate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(newsWaitTime);
+            newsText.text = _newsLines[Random.Range(0, _newsLines.Length)];
+        }
+    }
 }
